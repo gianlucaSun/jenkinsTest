02305c62e23f4eb4d4a22bbc205ff21df15196e7
g.germani <gianluca.germani.consulente@rai.it>
2026-04-15 11:13:58 +0200

    gg mod

--- src/main/java/it/gg/MainTest.java
+++ src/main/java/it/gg/MainTest.java
@@ -3,7 +3,7 @@ package it.gg;
 public class MainTest {
 
 	public static void main(String[] args) {
-		System.out.println("CiaooooXXX test Jenkins...");
+		System.out.println("CiaooooXXX test Jenkins 8...");
 
 	}
 
