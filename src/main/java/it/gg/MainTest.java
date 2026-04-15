f0e01f47eec3a39740400d4b181e2480ce0debed
g.germani <gianluca.germani.consulente@rai.it>
2026-04-15 12:11:39 +0200

    gg mod 13

--- src/main/java/it/gg/MainTest.java
+++ src/main/java/it/gg/MainTest.java
@@ -3,7 +3,7 @@ package it.gg;
 public class MainTest {
 
 	public static void main(String[] args) {
-		System.out.println("CiaooooXXX test Jenkins 11...");
+		System.out.println("CiaooooXXX test Jenkins 13...");
 
 	}
 
